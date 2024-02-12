0b80a7a1876232da6a459ed4be5e890e6b72050b
Franco Silva <franco_tigres18@hotmail.com>
2024-02-11 22:30:04 -0600

    feat(QuioscoContext)

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,14 +1,22 @@
+import useQuiosco from "../hooks/useQuiosco";
+
 export default function Categoria({ categoria }) {
 
+  const { handleClickCategoria, categoriaActual } = useQuiosco()
   const { nombre, icono, id } = categoria
 
+  const resaltarCategoriaActual = () => categoriaActual.id === id ? 'bg-amber-400' : 'bg-white'
+
   return (
-    <div className="flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer">
+    <div className={`${resaltarCategoriaActual()} flex items-center gap-4 border w-full p-3 hover:bg-amber-400 cursor-pointer`}>
       <img src={`/img/icono_${icono}.svg`} alt="Imagen Icono"
         className="w-12"
       />
 
-      <p className="text-lg font-bold cursor-pointer truncate">{nombre}</p>
+      <button
+        type="button"
+        onClick={() => handleClickCategoria(id)}
+        className="text-lg font-bold cursor-pointer truncate">{nombre}</button>
     </div>
   );
 }
